ab1f24cdf9f0b91d061033bb1562531ec55fd6df
Francesca-palmeri <francesca.palmeri.c@gmail.com>
2024-11-14 16:34:24 +0100

    primo

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const tombolone = document.getElementById("grigliaTombola");
+
+document.addEventListener('load', init());
+
+function init(){
+    grigliaTombolone();
+}
+
+function grigliaTombolone() {
+    
+}
